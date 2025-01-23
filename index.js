310ad30df210ae702d6dd2c86b406f4de89e60db
sanavulla shaik <sanavullashaik0@gmail.com>
2025-01-23 15:51:07 +0530

    update changes

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+var x = 1;
+a();
+b();
+console.log(s);
+function  a(){
+    var x = 7;
+    console.log(x);
+    
+}
+
+function b(){
+    var x = 8;
+    console.log(x);   
+}
+
+console.log(window.a)
+
+var s;
+console.log(s)
+s=10;
+console.log(s===undefined)
